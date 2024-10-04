ffe32e20f8bfe1d80183cee12fc7f71ceb59b855
Matsvei Kasiakou <matveykos04@gmail.com>
2024-10-04 21:04:03 +0300

    Lint fixes

--- src/generators/prima_generator.py
+++
@@ -1,9 +0,0 @@
-from maze import Maze
-from cell import Cell
-from type_of_cell import TypeOfCell
-from generators.generator import Generator
-
-
-class PrimaGenerator(Generator):
-    def generate(self, height: int, width: int) -> Maze:
-        pass
